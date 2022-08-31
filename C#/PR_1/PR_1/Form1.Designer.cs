07a6d8b30d56a6fd0ffa08a54f0797f08d6bdf70
Ivan Baluta <ltlaitoff@gmail.com>
2022-08-31 12:18:44 +0300

    feat: Created PR_1 VC#

---
+++ C#/PR_1/PR_1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PR_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(82, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 154);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Next:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Collection:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 264);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Practice work 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ C#/PR_1/PR_1/Form1.cs
@@ -0,0 +1,64 @@
+namespace PR_1
+{
+	public partial class Form1 : Form
+	{
+		private const string NOT_NUMBER = "-";
+		private const int MIN_COUNTER_VALUE = 0;
+		private const int MAX_COUNTER_VALUE = 9;
+		private const int NUMBER_VOID = -1;
+		private const string STRING_NUMBER = "{0} число";
+
+		private int counter = MIN_COUNTER_VALUE;
+
+		private int[] numbers = new int[MAX_COUNTER_VALUE] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+		public Form1()
+		{
+			InitializeComponent();
+			setNextText(getNumberString(numbers[counter]));
+		}
+
+		private void setNextText(string value)
+		{
+			textBox1.Text = value;
+		}
+
+		private void setPrevText(string value)
+		{
+			listBox1.Items.Add(value);
+		}
+
+		private int getNextCounterValue(int currentCounterValue)
+		{
+			int nextCounterValue = ++currentCounterValue;
+
+			if (nextCounterValue > MAX_COUNTER_VALUE - 1) return NUMBER_VOID;
+
+			return nextCounterValue;
+		}
+
+		private string getNumberString(int value)
+		{
+			if (value == NUMBER_VOID) return NOT_NUMBER;
+
+			return String.Format(STRING_NUMBER, value);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (counter < MIN_COUNTER_VALUE) return;
+
+			setPrevText(getNumberString(numbers[counter]));
+
+			counter = getNextCounterValue(counter);
+
+			if (counter == NUMBER_VOID)
+			{
+				setNextText(getNumberString(NUMBER_VOID));
+				return;
+			}
+
+			setNextText(getNumberString(numbers[counter]));
+		}
+	}
+}
