6b76fa8f758e865f792b3d27112b8f59e99e653f
ltlaitoff <ltlaitoff@gmail.com>
2023-01-12 10:39:15 +0200

    feat: Added connect vue-axios and axios in main.js

--- practices/practice-1/part-2/day-5/src/main.js
+++ practices/practice-1/part-2/day-5/src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import './style/main.scss'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueAxios, axios)
 
 app.mount('#app')
