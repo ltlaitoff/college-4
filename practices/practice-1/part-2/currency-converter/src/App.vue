03bc2cd28d383d9565163263ea02b72d7ac16b95
ltlaitoff <ltlaitoff@gmail.com>
2023-01-12 21:30:35 +0200

    feat: Monobank API, tailwind and all done

--- practices/practice-1/part-2/currency-converter/src/App.vue
+++ practices/practice-1/part-2/currency-converter/src/App.vue
@@ -1,5 +1,124 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, computed } from 'vue'
+import { getCourse } from './api/api'
 
-<template>Test</template>
+const currencyCourses = ref([])
+const fromCurrency = ref('980')
+const toCurrency = ref('840')
+const value = ref(1)
+
+const findCourse = () => {
+	return currencyCourses.value.find(item => {
+		if (
+			item.currencyCodeA == fromCurrency.value &&
+			item.currencyCodeB == toCurrency.value
+		)
+			return item
+
+		if (
+			item.currencyCodeA == toCurrency.value &&
+			item.currencyCodeB == fromCurrency.value
+		)
+			return item
+	})
+}
+
+onMounted(() => {
+	getCourse().then(value => {
+		currencyCourses.value = value
+	})
+})
+
+const getCurrencyName = code => {
+	const data = {
+		978: 'EUR',
+		840: 'USD',
+		980: 'UAH'
+	}
+
+	return data[code]
+}
+
+const buyValue = computed(() => {
+	if (fromCurrency.value === toCurrency.value) return 1
+
+	const currentCourse = findCourse()
+	if (currentCourse === undefined) return
+
+	if (currentCourse.currencyCodeA == fromCurrency.value)
+		return currentCourse.rateBuy
+
+	return 1 / currentCourse.rateBuy
+})
+
+const saleValue = computed(() => {
+	if (fromCurrency.value === toCurrency.value) return 1
+
+	const currentCourse = findCourse()
+	if (currentCourse === undefined) return
+
+	if (currentCourse.currencyCodeA == fromCurrency.value)
+		return currentCourse.rateSell
+
+	return 1 / currentCourse.rateSell
+})
+</script>
+
+<template>
+	<main class="m-10">
+		<label>
+			{{ getCurrencyName(fromCurrency) }}:
+			<input
+				class="border rounded border-gray-500 pl-2"
+				type="number"
+				v-model.number="value"
+			/>
+		</label>
+
+		<div class="mt-5">
+			{{ getCurrencyName(toCurrency) }}:
+			<label>
+				Buy:
+				<input
+					class="border rounded border-gray-500 pl-2 bg-gray-200"
+					type="text"
+					:value="(buyValue * value).toFixed(2)"
+					disabled
+				/>
+			</label>
+			<label>
+				Sale:
+				<input
+					class="border rounded border-gray-500 pl-2 bg-gray-200"
+					type="text"
+					:value="(saleValue * value).toFixed(2)"
+					disabled
+				/>
+			</label>
+		</div>
+
+		<div class="mt-5">
+			From currency
+			<select
+				v-model="fromCurrency"
+				class="border border-gray-300 rounded p-1"
+			>
+				<option value="980">UAH</option>
+				<option value="978">EUR</option>
+				<option value="840">USD</option>
+			</select>
+
+			To currency:
+			<select
+				v-model="toCurrency"
+				class="border border-gray-300 rounded p-1"
+			>
+				<option value="980">UAH</option>
+				<option value="978">EUR</option>
+				<option value="840">USD</option>
+			</select>
+		</div>
+	</main>
+</template>
 
 <style></style>
---
+++ practices/practice-1/part-2/currency-converter/src/api/api.js
@@ -0,0 +1,928 @@
+const a = [
+	{
+		currencyCodeA: 840,
+		currencyCodeB: 980,
+		date: 1673474474,
+		rateBuy: 36.65,
+		rateCross: 0,
+		rateSell: 37.4406
+	},
+	{
+		currencyCodeA: 978,
+		currencyCodeB: 980,
+		date: 1673546774,
+		rateBuy: 39.6,
+		rateCross: 0,
+		rateSell: 40.7498
+	},
+	{
+		currencyCodeA: 978,
+		currencyCodeB: 840,
+		date: 1673546774,
+		rateBuy: 1.074,
+		rateCross: 0,
+		rateSell: 1.089
+	},
+	{
+		currencyCodeA: 826,
+		currencyCodeB: 980,
+		date: 1673548991,
+		rateBuy: 0,
+		rateCross: 45.6092,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 392,
+		currencyCodeB: 980,
+		date: 1673548708,
+		rateBuy: 0,
+		rateCross: 0.2835,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 756,
+		currencyCodeB: 980,
+		date: 1673548988,
+		rateBuy: 0,
+		rateCross: 40.6495,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 156,
+		currencyCodeB: 980,
+		date: 1673547774,
+		rateBuy: 0,
+		rateCross: 5.5339,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 784,
+		currencyCodeB: 980,
+		date: 1673548836,
+		rateBuy: 0,
+		rateCross: 10.1953,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 971,
+		currencyCodeB: 980,
+		date: 1663425223,
+		rateBuy: 0,
+		rateCross: 0.4252,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 8,
+		currencyCodeB: 980,
+		date: 1673548974,
+		rateBuy: 0,
+		rateCross: 0.347,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 51,
+		currencyCodeB: 980,
+		date: 1673548811,
+		rateBuy: 0,
+		rateCross: 0.0954,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 973,
+		currencyCodeB: 980,
+		date: 1673515717,
+		rateBuy: 0,
+		rateCross: 0.0743,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 32,
+		currencyCodeB: 980,
+		date: 1673548988,
+		rateBuy: 0,
+		rateCross: 0.1205,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 36,
+		currencyCodeB: 980,
+		date: 1673548646,
+		rateBuy: 0,
+		rateCross: 25.9411,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 944,
+		currencyCodeB: 980,
+		date: 1673548993,
+		rateBuy: 0,
+		rateCross: 22.0244,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 50,
+		currencyCodeB: 980,
+		date: 1673548825,
+		rateBuy: 0,
+		rateCross: 0.3596,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 975,
+		currencyCodeB: 980,
+		date: 1673548980,
+		rateBuy: 0,
+		rateCross: 20.6448,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 48,
+		currencyCodeB: 980,
+		date: 1673526133,
+		rateBuy: 0,
+		rateCross: 99.5608,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 108,
+		currencyCodeB: 980,
+		date: 1538606522,
+		rateBuy: 0,
+		rateCross: 0.0158,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 96,
+		currencyCodeB: 980,
+		date: 1673166020,
+		rateBuy: 0,
+		rateCross: 28.08,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 68,
+		currencyCodeB: 980,
+		date: 1673023782,
+		rateBuy: 0,
+		rateCross: 5.4578,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 986,
+		currencyCodeB: 980,
+		date: 1673548776,
+		rateBuy: 0,
+		rateCross: 7.2238,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 72,
+		currencyCodeB: 980,
+		date: 1670477598,
+		rateBuy: 0,
+		rateCross: 2.9174,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 933,
+		currencyCodeB: 980,
+		date: 1673540742,
+		rateBuy: 0,
+		rateCross: 13.977,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 124,
+		currencyCodeB: 980,
+		date: 1673548999,
+		rateBuy: 0,
+		rateCross: 27.9333,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 976,
+		currencyCodeB: 980,
+		date: 1655462332,
+		rateBuy: 0,
+		rateCross: 0.0163,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 152,
+		currencyCodeB: 980,
+		date: 1673547615,
+		rateBuy: 0,
+		rateCross: 0.0454,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 170,
+		currencyCodeB: 980,
+		date: 1673544739,
+		rateBuy: 0,
+		rateCross: 0.0077,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 188,
+		currencyCodeB: 980,
+		date: 1673542369,
+		rateBuy: 0,
+		rateCross: 0.0644,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 192,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 1.5237,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 203,
+		currencyCodeB: 980,
+		date: 1673548991,
+		rateBuy: 0,
+		rateCross: 1.6819,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 262,
+		currencyCodeB: 980,
+		date: 1673264091,
+		rateBuy: 0,
+		rateCross: 0.2109,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 208,
+		currencyCodeB: 980,
+		date: 1673548810,
+		rateBuy: 0,
+		rateCross: 5.4251,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 12,
+		currencyCodeB: 980,
+		date: 1673525812,
+		rateBuy: 0,
+		rateCross: 0.2745,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 818,
+		currencyCodeB: 980,
+		date: 1673548941,
+		rateBuy: 0,
+		rateCross: 1.2634,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 230,
+		currencyCodeB: 980,
+		date: 1673088334,
+		rateBuy: 0,
+		rateCross: 0.6993,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 981,
+		currencyCodeB: 980,
+		date: 1673548772,
+		rateBuy: 0,
+		rateCross: 14.1532,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 936,
+		currencyCodeB: 980,
+		date: 1673538648,
+		rateBuy: 0,
+		rateCross: 3.2558,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 270,
+		currencyCodeB: 980,
+		date: 1673527593,
+		rateBuy: 0,
+		rateCross: 0.6137,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 324,
+		currencyCodeB: 980,
+		date: 1669466935,
+		rateBuy: 0,
+		rateCross: 0.0043,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 344,
+		currencyCodeB: 980,
+		date: 1673546874,
+		rateBuy: 0,
+		rateCross: 4.7962,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 191,
+		currencyCodeB: 980,
+		date: 1673404745,
+		rateBuy: 0,
+		rateCross: 5.318,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 348,
+		currencyCodeB: 980,
+		date: 1673548975,
+		rateBuy: 0,
+		rateCross: 0.1013,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 360,
+		currencyCodeB: 980,
+		date: 1673548253,
+		rateBuy: 0,
+		rateCross: 0.0024,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 376,
+		currencyCodeB: 980,
+		date: 1673548872,
+		rateBuy: 0,
+		rateCross: 10.9257,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 356,
+		currencyCodeB: 980,
+		date: 1673546653,
+		rateBuy: 0,
+		rateCross: 0.4594,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 368,
+		currencyCodeB: 980,
+		date: 1673545185,
+		rateBuy: 0,
+		rateCross: 0.0256,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 364,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 0.0009,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 352,
+		currencyCodeB: 980,
+		date: 1673548484,
+		rateBuy: 0,
+		rateCross: 0.2606,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 400,
+		currencyCodeB: 980,
+		date: 1673548310,
+		rateBuy: 0,
+		rateCross: 52.885,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 404,
+		currencyCodeB: 980,
+		date: 1673544511,
+		rateBuy: 0,
+		rateCross: 0.3027,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 417,
+		currencyCodeB: 980,
+		date: 1673545817,
+		rateBuy: 0,
+		rateCross: 0.4371,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 116,
+		currencyCodeB: 980,
+		date: 1673323935,
+		rateBuy: 0,
+		rateCross: 0.009,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 408,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 16.6221,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 410,
+		currencyCodeB: 980,
+		date: 1673543092,
+		rateBuy: 0,
+		rateCross: 0.0302,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 414,
+		currencyCodeB: 980,
+		date: 1673533038,
+		rateBuy: 0,
+		rateCross: 122.5333,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 398,
+		currencyCodeB: 980,
+		date: 1673548776,
+		rateBuy: 0,
+		rateCross: 0.0811,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 418,
+		currencyCodeB: 980,
+		date: 1673521843,
+		rateBuy: 0,
+		rateCross: 0.0021,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 422,
+		currencyCodeB: 980,
+		date: 1672238942,
+		rateBuy: 0,
+		rateCross: 0.0009,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 144,
+		currencyCodeB: 980,
+		date: 1673546905,
+		rateBuy: 0,
+		rateCross: 0.1024,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 434,
+		currencyCodeB: 980,
+		date: 1648958626,
+		rateBuy: 0,
+		rateCross: 6.4735,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 504,
+		currencyCodeB: 980,
+		date: 1673546571,
+		rateBuy: 0,
+		rateCross: 3.6717,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 498,
+		currencyCodeB: 980,
+		date: 1673548990,
+		rateBuy: 0,
+		rateCross: 1.9635,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 969,
+		currencyCodeB: 980,
+		date: 1672132750,
+		rateBuy: 0,
+		rateCross: 0.0084,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 807,
+		currencyCodeB: 980,
+		date: 1673548257,
+		rateBuy: 0,
+		rateCross: 0.6535,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 496,
+		currencyCodeB: 980,
+		date: 1673511763,
+		rateBuy: 0,
+		rateCross: 0.0109,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 478,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 0.101,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 480,
+		currencyCodeB: 980,
+		date: 1673539589,
+		rateBuy: 0,
+		rateCross: 0.855,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 454,
+		currencyCodeB: 980,
+		date: 1633949773,
+		rateBuy: 0,
+		rateCross: 0.0325,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 484,
+		currencyCodeB: 980,
+		date: 1673548881,
+		rateBuy: 0,
+		rateCross: 1.9806,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 458,
+		currencyCodeB: 980,
+		date: 1673548929,
+		rateBuy: 0,
+		rateCross: 8.5705,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 943,
+		currencyCodeB: 980,
+		date: 1673187952,
+		rateBuy: 0,
+		rateCross: 0.5919,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 516,
+		currencyCodeB: 980,
+		date: 1673545460,
+		rateBuy: 0,
+		rateCross: 2.2132,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 566,
+		currencyCodeB: 980,
+		date: 1673548942,
+		rateBuy: 0,
+		rateCross: 0.0812,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 558,
+		currencyCodeB: 980,
+		date: 1673544129,
+		rateBuy: 0,
+		rateCross: 1.0403,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 578,
+		currencyCodeB: 980,
+		date: 1673548892,
+		rateBuy: 0,
+		rateCross: 3.7626,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 524,
+		currencyCodeB: 980,
+		date: 1673537604,
+		rateBuy: 0,
+		rateCross: 0.2871,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 554,
+		currencyCodeB: 980,
+		date: 1673525011,
+		rateBuy: 0,
+		rateCross: 23.9205,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 512,
+		currencyCodeB: 980,
+		date: 1673547819,
+		rateBuy: 0,
+		rateCross: 97.2495,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 604,
+		currencyCodeB: 980,
+		date: 1673548770,
+		rateBuy: 0,
+		rateCross: 9.9059,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 608,
+		currencyCodeB: 980,
+		date: 1673545272,
+		rateBuy: 0,
+		rateCross: 0.6806,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 586,
+		currencyCodeB: 980,
+		date: 1673547434,
+		rateBuy: 0,
+		rateCross: 0.164,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 985,
+		currencyCodeB: 980,
+		date: 1673549000,
+		rateBuy: 0,
+		rateCross: 8.6216,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 600,
+		currencyCodeB: 980,
+		date: 1673548560,
+		rateBuy: 0,
+		rateCross: 0.005,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 634,
+		currencyCodeB: 980,
+		date: 1673548472,
+		rateBuy: 0,
+		rateCross: 10.2863,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 946,
+		currencyCodeB: 980,
+		date: 1673548994,
+		rateBuy: 0,
+		rateCross: 8.176,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 941,
+		currencyCodeB: 980,
+		date: 1673548572,
+		rateBuy: 0,
+		rateCross: 0.3434,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 682,
+		currencyCodeB: 980,
+		date: 1673546425,
+		rateBuy: 0,
+		rateCross: 9.9725,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 690,
+		currencyCodeB: 980,
+		date: 1673535719,
+		rateBuy: 0,
+		rateCross: 2.6843,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 938,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 0.0631,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 752,
+		currencyCodeB: 980,
+		date: 1673548974,
+		rateBuy: 0,
+		rateCross: 3.6037,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 702,
+		currencyCodeB: 980,
+		date: 1673548478,
+		rateBuy: 0,
+		rateCross: 28.1808,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 694,
+		currencyCodeB: 980,
+		date: 1664217991,
+		rateBuy: 0,
+		rateCross: 0.0024,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 706,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 0.0647,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 968,
+		currencyCodeB: 980,
+		date: 1669419358,
+		rateBuy: 0,
+		rateCross: 1.223,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 760,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 0.0081,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 748,
+		currencyCodeB: 980,
+		date: 1668614714,
+		rateBuy: 0,
+		rateCross: 2.1898,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 764,
+		currencyCodeB: 980,
+		date: 1673548720,
+		rateBuy: 0,
+		rateCross: 1.1264,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 972,
+		currencyCodeB: 980,
+		date: 1673533665,
+		rateBuy: 0,
+		rateCross: 3.6578,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 795,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 0.0021,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 788,
+		currencyCodeB: 980,
+		date: 1673539811,
+		rateBuy: 0,
+		rateCross: 12.3442,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 949,
+		currencyCodeB: 980,
+		date: 1673548975,
+		rateBuy: 0,
+		rateCross: 2.0128,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 901,
+		currencyCodeB: 980,
+		date: 1673544720,
+		rateBuy: 0,
+		rateCross: 1.2312,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 834,
+		currencyCodeB: 980,
+		date: 1673548631,
+		rateBuy: 0,
+		rateCross: 0.0162,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 800,
+		currencyCodeB: 980,
+		date: 1673535479,
+		rateBuy: 0,
+		rateCross: 0.0101,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 858,
+		currencyCodeB: 980,
+		date: 1673546198,
+		rateBuy: 0,
+		rateCross: 0.9418,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 860,
+		currencyCodeB: 980,
+		date: 1673547999,
+		rateBuy: 0,
+		rateCross: 0.0033,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 937,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 1.9241,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 704,
+		currencyCodeB: 980,
+		date: 1673538908,
+		rateBuy: 0,
+		rateCross: 0.0015,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 950,
+		currencyCodeB: 980,
+		date: 1673537694,
+		rateBuy: 0,
+		rateCross: 0.0613,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 952,
+		currencyCodeB: 980,
+		date: 1673541865,
+		rateBuy: 0,
+		rateCross: 0.0615,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 886,
+		currencyCodeB: 980,
+		date: 1543715495,
+		rateBuy: 0,
+		rateCross: 0.112,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 710,
+		currencyCodeB: 980,
+		date: 1673548617,
+		rateBuy: 0,
+		rateCross: 2.2128,
+		rateSell: 0
+	},
+	{
+		currencyCodeA: 894,
+		currencyCodeB: 980,
+		date: 1673474406,
+		rateBuy: 0,
+		rateCross: 0.002,
+		rateSell: 0
+	}
+]
+
+// const API_URL = 'https://api.monobank.ua/bank/currency'
+
+export const getCourse = () => {
+	return new Promise(res => {
+		res(a)
+	})
+
+	// return fetch(API_URL)
+	// 	.then(responce => responce.json())
+	// 	.then(() => {
+	// 		return a
+	// 	})
+}
