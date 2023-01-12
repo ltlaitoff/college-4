09be19b9990d06c1590e7cd640d99c878adba076
ltlaitoff <ltlaitoff@gmail.com>
2023-01-12 16:57:42 +0200

    feat: Created AddStudentForm component

---
+++ practices/practice-1/part-2/day-5/src/components/AddStudentForm.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { reactive, defineEmits } from 'vue'
+
+const emits = defineEmits({
+	addStudent: null
+})
+
+const state = reactive({
+	name: '',
+	group: '',
+	photo: '',
+	mark: 0,
+	isDonePr: false
+})
+
+const stateToObject = state => {
+	return {
+		_id: String(Date.now()),
+		name: state.name,
+		group: state.group,
+		photo: state.photo,
+		mark: state.mark,
+		isDonePr: state.isDonePr
+	}
+}
+
+function addStudent() {
+	emits('addStudent', stateToObject(state))
+}
+</script>
+
+<template>
+	<form
+		class="addStudent"
+		@submit.prevent="addStudent"
+	>
+		<label class="addStudentLabel">
+			Name
+			<input
+				type="text"
+				v-model="state.name"
+			/>
+		</label>
+
+		<label class="addStudentLabel">
+			Group
+			<select
+				class="select"
+				v-model="state.group"
+			>
+				<option value="1">1</option>
+				<option value="2">2</option>
+			</select>
+		</label>
+
+		<label class="addStudentLabel">
+			Practive is done
+			<input
+				v-model="state.isDonePr"
+				type="checkbox"
+			/>
+		</label>
+
+		<button class="addStudentButton">Add student</button>
+	</form>
+</template>
+
+<style lang="scss"></style>
