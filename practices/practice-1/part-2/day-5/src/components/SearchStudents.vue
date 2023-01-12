9c9f66114c549c4e4bdf2828c1d4f7cce219d28d
ltlaitoff <ltlaitoff@gmail.com>
2023-01-12 17:34:33 +0200

    style: SearchStudents styles

--- practices/practice-1/part-2/day-5/src/components/SearchStudents.vue
+++ practices/practice-1/part-2/day-5/src/components/SearchStudents.vue
@@ -15,8 +15,12 @@ const value = computed({
 </script>
 
 <template>
-	<input
-		type="text"
-		v-model="value"
-	/>
+	<label class="font-light">
+		Search:
+		<input
+			class="text-base ml-2 border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:border-sky-300 focus-visible:outline-none p-1 pl-2"
+			type="text"
+			v-model="value"
+		/>
+	</label>
 </template>
