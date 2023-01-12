f009b51162a7fa070dbbd042e9d5adb610053d8b
ltlaitoff <ltlaitoff@gmail.com>
2023-01-12 16:58:01 +0200

    feat: Created component SearchStudents

---
+++ practices/practice-1/part-2/day-5/src/components/SearchStudents.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+	get() {
+		return props.modelValue
+	},
+	set(value) {
+		emit('update:modelValue', value)
+	}
+})
+</script>
+
+<template>
+	<input
+		type="text"
+		v-model="value"
+	/>
+</template>
+
+<style lang="scss" scoped></style>
