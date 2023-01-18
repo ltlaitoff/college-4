8909e7e043c876251e170313b2848842d631183d
ltlaitoff <ltlaitoff@gmail.com>
2023-01-18 13:45:16 +0200

    feat: Removed logo.svg

--- practices/practice-1/part-2/independent-work-weather/src/App.vue
+++ practices/practice-1/part-2/independent-work-weather/src/App.vue
@@ -4,14 +4,6 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
