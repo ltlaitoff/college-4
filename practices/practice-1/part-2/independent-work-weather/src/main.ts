185c57a799af8d72ae1ee8000f63c47f2c97fa62
ltlaitoff <ltlaitoff@gmail.com>
2023-01-18 13:42:07 +0200

    refactor: Moved tailwind.css to style folder

--- practices/practice-1/part-2/independent-work-weather/src/main.ts
+++ practices/practice-1/part-2/independent-work-weather/src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-import './assets/tailwind.css'
+import './style/tailwind.css'
 
 const app = createApp(App)
 
