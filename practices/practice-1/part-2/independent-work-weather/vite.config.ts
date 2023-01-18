2a47229bb3f1c77e97d5e1fa09d371fc02d63516
ltlaitoff <ltlaitoff@gmail.com>
2023-01-18 16:40:17 +0200

    feat: Using VITE_TITLE_APP in index.html

--- practices/practice-1/part-2/independent-work-weather/vite.config.ts
+++ practices/practice-1/part-2/independent-work-weather/vite.config.ts
@@ -1,14 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      vue(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            title: env.VITE_APP_TITLE
+          }
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
