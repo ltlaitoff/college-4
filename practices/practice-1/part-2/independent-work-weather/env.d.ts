73b9045fbb21dc2ae0bdfc15a7fddb71619c91ca
ltlaitoff <ltlaitoff@gmail.com>
2023-01-18 16:38:33 +0200

    chore: Created env files

--- practices/practice-1/part-2/independent-work-weather/env.d.ts
+++ practices/practice-1/part-2/independent-work-weather/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_API_KEY: string
+  readonly VITE_API_CURRENT_WEATHER_HOST: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
