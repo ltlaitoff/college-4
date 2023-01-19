7b8a46af849b6efe6ee18dbeed66524cdc1d889b
ltlaitoff <ltlaitoff@gmail.com>
2023-01-19 19:01:55 +0200

    feat: Added AddCityView in router

--- practices/practice-1/part-2/independent-work-weather/src/router/index.ts
+++ practices/practice-1/part-2/independent-work-weather/src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddCityView from '../views/AddCityView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
+    },
+    {
+      path: '/add-city',
+      name: 'Add city',
+      component: AddCityView
     }
   ]
 })
