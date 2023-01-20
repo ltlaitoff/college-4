ea8104cb35af05c53a572483acb84c75ae49157c
ltlaitoff <ltlaitoff@gmail.com>
2023-01-20 19:49:51 +0200

    feat: Created AddCityView

--- practices/practice-1/part-2/independent-work-weather/src/views/AddCityView.vue
+++ practices/practice-1/part-2/independent-work-weather/src/views/AddCityView.vue
@@ -1,5 +1,92 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getCitiesListByName } from '@/api'
+import type { CityStorageItem } from '@/types/storage'
+import { ref, watch } from 'vue'
+import getUnicodeFlagIcon from 'country-flag-icons/unicode'
+import { useTimer } from '../composables/useTimer'
+import { useCitiesStore } from '../stores/cities'
+
+const inputValue = ref('')
+const searchItems = ref<Array<CityStorageItem> | null>(null)
+const showSelectList = ref<boolean>(false)
+const cityAdded = ref<boolean>(false)
+
+const timer = useTimer(500, () => {
+  getCitiesListByName(inputValue.value).then(value => {
+    console.log(value)
+    searchItems.value = value
+  })
+
+  if (cityAdded.value) {
+    cityAdded.value = false
+  }
+
+  console.log(inputValue.value)
+})
+
+watch(
+  () => inputValue.value,
+  () => {
+    timer.restart()
+  }
+)
+
+const citiesStore = useCitiesStore()
+
+const onItemClick = (item: CityStorageItem) => {
+  inputValue.value =
+    item.name + ' ' + item.country + ' ' + `${item.state || ''}`
+
+  showSelectList.value = false
+
+  citiesStore.addCity(item)
+  cityAdded.value = true
+}
+
+const onInputFocus = () => {
+  showSelectList.value = true
+}
+
+const mainElementRef = ref<HTMLLabelElement | null>(null)
+</script>
 
 <template>
-  <main>AddCity View</main>
+  <div
+    ref="mainElementRef"
+    class="inline-flex gap-x-3"
+  >
+    <span class="py-2">Choice city for adding:</span>
+    <div class="w-72 relative">
+      <input
+        :class="[
+          `border rounded border-gray-400 py-2 px-4 w-full`,
+          { 'border-green-500 bg-green-100': cityAdded }
+        ]"
+        type="text"
+        v-model="inputValue"
+        @focus="onInputFocus"
+      />
+      <div
+        class="absolute flex flex-col mt-2 w-72 border rounded p-1 gap-y-1 bg-white"
+        v-if="
+          showSelectList && searchItems !== null && searchItems.length !== 0
+        "
+      >
+        <button
+          class="p-2 flex gap-x-5 justify-between hover:bg-pink-200 rounded items-center"
+          v-for="searchItem in searchItems"
+          :key="searchItem.name"
+          @click="onItemClick(searchItem)"
+        >
+          <div class="font-medium">
+            {{ searchItem.name }}
+          </div>
+          <div class="flex gap-x-2 flex-wrap">
+            {{ searchItem.country }}
+            <span v-if="searchItem.state">{{ searchItem.state }}</span>
+          </div>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
