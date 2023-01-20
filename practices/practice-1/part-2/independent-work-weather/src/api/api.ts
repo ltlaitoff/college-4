c2a428805ac20ae902c01df64cd8c3ab95c00b9b
ltlaitoff <ltlaitoff@gmail.com>
2023-01-20 12:22:02 +0200

    feat: Created getCityWeather API function

---
+++ practices/practice-1/part-2/independent-work-weather/src/api/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+import type { CityStorageItem } from '@/types/storage'
+
+const API_KEY = import.meta.env.VITE_API_KEY
+
+export const getCityWeather = (cityInfo: CityStorageItem) => {
+  return axios
+    .get(
+      `/weather?lat=${cityInfo.lat}&lon=${cityInfo.lon}&appid=${API_KEY}&lang=en`
+    )
+    .then(response => response.data)
+}
