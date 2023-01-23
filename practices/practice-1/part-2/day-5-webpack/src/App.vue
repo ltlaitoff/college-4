9f652ccd2d68a6317202608d4d3f6fac693ff66d
ltlaitoff <ltlaitoff@gmail.com>
2023-01-23 09:56:52 +0200

    feat: Using themeStore

--- practices/practice-1/part-2/day-5-webpack/src/App.vue
+++ practices/practice-1/part-2/day-5-webpack/src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { computed } from 'vue'
 import { RouterView } from 'vue-router'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const themeIsDark = computed(() => {
+	return store.getters['theme/getTheme'] === 'dark'
+})
+/* */
 </script>
 
 <template>
-	<main class="box-border m-10">
-		<RouterView />
+	<main :class="['h-full', themeIsDark && 'dark']">
+		<div class="box-border p-10 bg-white dark:bg-gray-400 h-full">
+			<RouterView />
+		</div>
 	</main>
 </template>
 
