1fbb9246fdf8160857c79156d3c02d477ec24ede
ltlaitoff <ltlaitoff@gmail.com>
2023-01-23 10:14:36 +0200

    style: Dark mode SearchStudents

--- practices/practice-1/part-2/day-5-webpack/src/components/SearchStudents.vue
+++ practices/practice-1/part-2/day-5-webpack/src/components/SearchStudents.vue
@@ -15,10 +15,10 @@ const value = computed({
 </script>
 
 <template>
-	<label class="font-light">
+	<label class="font-light text-gray-800 dark:text-gray-100">
 		Search:
 		<input
-			class="text-base ml-2 border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:border-sky-300 focus-visible:outline-none p-1 pl-2"
+			class="text-base ml-2 border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:border-sky-300 focus-visible:outline-none p-1 pl-2 dark:border-gray-500 dark:bg-gray-500 dark:font-normal dark:text-gray-100"
 			type="text"
 			v-model="value"
 		/>
