791e313c8274ce6b764029da3ad7f6ff06c05181
ltlaitoff <ltlaitoff@gmail.com>
2023-01-23 10:07:20 +0200

    style: Dark mode on AddStudentForm

--- practices/practice-1/part-2/day-5-webpack/src/components/AddStudentForm.vue
+++ practices/practice-1/part-2/day-5-webpack/src/components/AddStudentForm.vue
@@ -41,32 +41,38 @@ function addStudent() {
 </script>
 
 <template>
-	<div :class="$style.wrapper">
+	<div class="relative">
 		<button
-			:class="$style.openButton"
+			class="mt-3 hover:shadow-[0_0_20px_0_rgba(0,0,0,0.05)] text-gray-800 dark:text-gray-100 text-base font-light border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 dark:border-gray-500 rounded focus-visible:border-sky-300 focus-visible:outline-none py-2 px-4"
 			@click="isOpen = !isOpen"
 		>
 			{{ isOpen ? 'Close add form' : 'Open add form' }}
 		</button>
 		<form
 			v-if="isOpen"
-			:class="$style.form"
+			:class="[
+				'bg-white dark:bg-gray-600 p-2 pb-5 w-96 mt-5 inline-flex flex-col items-center justify-center border dark:border-gray-500 rounded',
+				$style.form
+			]"
 			@submit.prevent="addStudent"
 		>
-			<label :class="$style.label">
+			<label
+				class="flex items-center justify-between w-full py-3 px-4 text-slate-600 dark:text-slate-100 transition"
+			>
 				Name
 				<input
-					:class="$style.input"
+					class="w-48 text-base ml-2 border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:border-sky-300 focus-visible:outline-none p-1 pl-2"
 					type="text"
 					v-model="state.name"
 				/>
 			</label>
 
-			<label :class="$style.label">
+			<label
+				class="flex items-center justify-between w-full py-3 px-4 text-slate-600 dark:text-slate-100 transition"
+			>
 				Groups
 				<select
-					:class="$style.select"
-					class="select"
+					class="border-none py-2 px-4 bg-gray-100 text-base ml-2 border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:outline-none"
 					v-model="state.group"
 				>
 					<option
@@ -79,13 +85,13 @@ function addStudent() {
 					</option>
 
 					<option
-						:class="$style.selectOption"
+						class="bg-slate-200 border-none p-2"
 						value="RPZ 19 1/9"
 					>
 						RPZ 19 1/9
 					</option>
 					<option
-						:class="$style.selectOption"
+						class="bg-slate-200 border-none p-2"
 						value="RPZ 19 2/9"
 					>
 						RPZ 19 2/9
@@ -93,16 +99,22 @@ function addStudent() {
 				</select>
 			</label>
 
-			<label :class="$style.label">
+			<label
+				class="flex items-center justify-between w-full py-3 px-4 text-slate-600 dark:text-slate-100 transition"
+			>
 				Practice is done
 				<input
-					:class="$style.checkbox"
+					class="w-4 h-4"
 					v-model="state.isDonePr"
 					type="checkbox"
 				/>
 			</label>
 
-			<button :class="$style.addButton">Add student</button>
+			<button
+				class="mt-5 hover:border-green-400 hover:shadow-[0_0_20px_0_rgba(0,0,0,0.2)] py-2 px-4 shadow bg-zinc-100 rounded border border-zinc-200 hover:bg-green-300 transition text-zinc-600 hover:text-zinc-800"
+			>
+				Add student
+			</button>
 		</form>
 	</div>
 </template>
@@ -112,40 +124,11 @@ function addStudent() {
 	position: relative;
 }
 
-.openButton {
-	@apply mt-3 hover:shadow-[0_0_20px_0_rgba(0,0,0,0.05)] text-gray-800 text-base font-light  border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:border-sky-300 focus-visible:outline-none py-2 px-4;
-}
-
 .form {
 	position: absolute;
 	top: calc(35px + 0.75em);
 	left: 0;
 
 	z-index: 99;
-	@apply bg-white p-2 pb-5 w-96 mt-5 inline-flex flex-col items-center justify-center items-start border rounded;
-}
-
-.label {
-	@apply flex items-center justify-between w-full py-3 px-4 text-slate-600 transition;
-}
-
-.input {
-	@apply w-48 text-base ml-2 border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:border-sky-300 focus-visible:outline-none p-1 pl-2;
-}
-
-.select {
-	@apply border-none py-2 px-4 bg-gray-100 text-base ml-2 border focus-visible:shadow-[0_0_20px_0_rgba(0,0,0,0.15)] transition border-neutral-300 rounded focus-visible:outline-none;
-}
-
-.selectOption {
-	@apply bg-slate-200 border-none p-2;
-}
-
-.checkbox {
-	@apply w-4 h-4;
-}
-
-.addButton {
-	@apply mt-5 hover:border-green-400 hover:shadow-[0_0_20px_0_rgba(0,0,0,0.2)] py-2 px-4 shadow bg-zinc-100 rounded border border-zinc-200 hover:bg-green-300 transition text-zinc-600 hover:text-zinc-800;
 }
 </style>
