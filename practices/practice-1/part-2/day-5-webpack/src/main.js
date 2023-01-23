7dfabff4eaf26015e314c7b46f952329663d05fd
ltlaitoff <ltlaitoff@gmail.com>
2023-01-23 09:17:19 +0200

    feat: Connect store to app

--- practices/practice-1/part-2/day-5-webpack/src/main.js
+++ practices/practice-1/part-2/day-5-webpack/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { store } from './store'
 
 import './style/main.css'
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(VueAxios, axios)
+app.use(store)
 
 app.mount('#app')
