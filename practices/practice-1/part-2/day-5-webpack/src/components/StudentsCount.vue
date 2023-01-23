9179a37c1e025feb0e4db18b3226ea6fe3668e93
ltlaitoff <ltlaitoff@gmail.com>
2023-01-23 10:16:04 +0200

    style: Dark mode StudentsCount

--- practices/practice-1/part-2/day-5-webpack/src/components/StudentsCount.vue
+++ practices/practice-1/part-2/day-5-webpack/src/components/StudentsCount.vue
@@ -9,10 +9,10 @@ const props = defineProps({
 </script>
 
 <template>
-	<div class="mt-2 p-2 text-gray-600">
+	<div class="mt-2 p-2 text-gray-600 dark:text-gray-200">
 		Count:
 		<span
-			class="bg-clip-text font-medium text-transparent bg-gradient-to-r from-pink-600 to-red-700"
+			class="bg-clip-text font-medium text-transparent bg-gradient-to-r from-pink-600 to-red-700 dark:from-pink-400 dark:to-red-500"
 		>
 			{{ props.count }}
 		</span>
