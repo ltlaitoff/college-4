5197ac5651f110cd13752ee7a7e90ae1f9a53309
ltlaitoff <ltlaitoff@gmail.com>
2023-01-23 09:33:32 +0200

    feat: Created StudentsCount component

---
+++ practices/practice-1/part-2/day-5-webpack/src/components/StudentsCount.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+	count: {
+		type: Number,
+		required: true
+	}
+})
+/* */
+</script>
+
+<template>
+	<div class="mt-2 p-2 text-gray-600">
+		Count:
+		<span
+			class="bg-clip-text font-medium text-transparent bg-gradient-to-r from-pink-600 to-red-700"
+		>
+			{{ props.count }}
+		</span>
+	</div>
+</template>
