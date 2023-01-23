3c59d986db2d58e5fa26b3be506f8bb20658bbde
ltlaitoff <ltlaitoff@gmail.com>
2023-01-23 10:50:37 +0200

    feat: Connect vuex-persistedstate to store

--- practices/practice-1/part-2/day-5-webpack/src/store/store.js
+++ practices/practice-1/part-2/day-5-webpack/src/store/store.js
@@ -1,8 +1,15 @@
 import { createStore } from 'vuex'
 import { countStore, themeStore } from './modules'
+/* WARNING: vuex-persistedstate is DEPRECATED */
+import createPersistedState from 'vuex-persistedstate'
 
 const store = createStore({
-	modules: { count: countStore, theme: themeStore }
+	modules: { count: countStore, theme: themeStore },
+
+	plugins: [
+		/* WARNING: vuex-persistedstate is DEPRECATED */
+		createPersistedState({ key: 'students-app' })
+	]
 })
 
 export { store }
